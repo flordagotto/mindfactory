8f0e3e02a08de66d18268c40bd1e0647014f4c0c
florencia dagotto <flopydagotto@gmail.com>
2025-09-20 18:35:52 -0300

    add dependency injections

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { AutomotoresModule } from './automotores/automotores.module';
 import { SujetosModule } from './sujetos/sujetos.module';
 import { ObjetoDeValorController } from './objeto-de-valor/objeto-de-valor.controller';
 import { ObjetoDeValorService } from './objeto-de-valor/objeto-de-valor.service';
 import { ObjetoDeValorModule } from './objeto-de-valor/objeto-de-valor.module';
+import { SujetosController } from './sujetos/sujetos.controller';
+import { SujetosService } from './sujetos/sujetos.service';
+import { AutomotoresService } from './automotores/automotores.service';
+import { AutomotoresController } from './automotores/automotores.controller';
 
 @Module({
   imports: [
@@ -24,7 +26,7 @@ import { ObjetoDeValorModule } from './objeto-de-valor/objeto-de-valor.module';
     SujetosModule,
     ObjetoDeValorModule,
   ],
-  controllers: [AppController, ObjetoDeValorController],
-  providers: [AppService, ObjetoDeValorService],
+  controllers: [SujetosController, ObjetoDeValorController, AutomotoresController],
+  providers: [SujetosService, ObjetoDeValorService, AutomotoresService],
 })
 export class AppModule {}
